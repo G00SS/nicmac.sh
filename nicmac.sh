6f091d19d06e9f8a0e26a85a0b80a8378d15cebf
G00SS <goussardnicolas@gmail.com>
2022-11-26 17:19:40 +0100

    Add files via upload

---
+++ nicmac.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##################################################################
+#  Récupération de l'adresse MAC de l'interface réseau d'une IP  #
+##################################################################
+#                                                                #
+#          SYNOPSYS :       ./mac.sh <IP.à.chercher>             #
+#                                                                #
+##################################################################
+
+# Si l'OS utilise ip
+if [[ -f "/bin/ip" || -f "/sbin/ip" ]]; then
+    # Récupération du numéro de la ligne concernant l'IP
+    linenum=`ip -br addr show | awk '/'$1'/{ print NR; exit }'`
+    # Récupération de la mac à cette même ligne dans une autre commande
+    nicmac=`ip -br link show | awk 'NR=="'$linenum'" {print;exit}' | awk '{print $3}'`
+
+# Sinon l'OS utilise ifconfig
+elif [ -f "/sbin/ifconfig" ]; then
+    nicmac=`ifconfig -a | awk -vRS= -vORS='\n\n' '/'$1'/' | grep "ether" | tr -s ' ' | cut -d ' ' -f3`
+
+# Sinon... Je ne sais pas
+else
+    nicmac=""
+fi
+
+# Affichage du résultat
+echo "$1"' <=> '"$nicmac"
